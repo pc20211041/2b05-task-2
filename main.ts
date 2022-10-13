13a6b9974bc0f74ef79d308fd433ead3019207f4
pc20211041 <115679800+pc20211041@users.noreply.github.com>
2022-10-13 11:01:29 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,52 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(WHEN_0 - 1)
+    basic.showNumber(WHEN_0 - 2)
+    basic.showNumber(WHEN_0 - 3)
+    basic.showNumber(WHEN_0 - 4)
+    basic.showNumber(WHEN_0 - 5)
+})
+let WHEN_0 = 0
+if (WHEN_0 == 0) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
